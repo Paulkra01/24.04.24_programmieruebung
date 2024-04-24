a5fa4565ed23874e228e8c58cc2fa8647d20b87f
Paulkra01 <krakis007@gmail.com>
2024-04-24 15:52:00 +0200

    add read_data file

--- 2/main.py
+++ 2/main.py
@@ -1,15 +1,18 @@
 import streamlit as st
+import read_data
+
 
 st.write("# EKG APP")
 st.write("## Versuchsperson auswählen")
+
 
 # Session State wird leer angelegt, solange er noch nicht existiert
 if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
 
-# Dieses Mal speichern wir die Auswahl als Session State
-st.session_state.current_user = st.selectbox(
-    'Versuchsperson',
-    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
+person_names = read_data.get_person_list()
 
-st.write("Der Name ist: ", st.session_state.current_user) 
+
+st.session_state.current_user = st.selectbox('Versuchsperson', options = person_names, key="sbVersuchsperson")
+
+st.write("Der Name ist: ", st.session_state.current_user)
--- 2/read_data.py
+++ 2/read_data.py
@@ -1,9 +1,15 @@
 import json
 
-# Opening JSON file
-file = open("data/person_db.json")
 
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
+def get_person_list():
+    # Opening JSON file
+    file = open("../data/person_db.json")
+    # Loading the JSON File in a dictionary
+    person_data = json.load(file)
+    Names = []
 
-person_data
+    for i in person_data:
+        name = [i["lastname"] + ", " +  i["firstname"]]
+        Names.append(name)
+    return Names
+
