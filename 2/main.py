1ea30f219502e85b4fab5da2d293714b4d87de25
Paulkra01 <krakis007@gmail.com>
2024-04-24 16:14:54 +0200

    update main file

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,7 @@
+from PIL import Image
 import streamlit as st
 import read_data
+
 
 
 st.write("# EKG APP")
@@ -16,3 +18,18 @@ person_names = read_data.get_person_list()
 st.session_state.current_user = st.selectbox('Versuchsperson', options = person_names, key="sbVersuchsperson")
 
 st.write("Der Name ist: ", st.session_state.current_user)
+
+
+# Auslesen des Pfades aus dem zurückgegebenen Dictionary
+current_picture_path = current_person["picture_path"]
+
+if 'picture_path' not in st.session_state:
+    st.session_state.picture_path = 'data/pictures/none.jpg'
+
+# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+if st.session_state.current_user in person_names:
+    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+
+# Öffne das Bild und Zeige es an
+image = Image.open("../" + st.session_state.picture_path)
+st.image(image, caption=st.session_state.current_user)
--- 2/read_data.py
+++ 2/read_data.py
@@ -9,6 +9,27 @@ def get_person_list():
     Names = []
 
     for i in person_data:
-        name = i['lastname'] + " " +  i['firstname']
+        name = i['lastname'] + ", " +  i['firstname']
         Names.append(name)
     return Names
+
+
+def find_person_data_by_name(suchstring):
+
+    file = open("../data/person_db.json")
+    person_data = json.load(file)
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for i in person_data:
+        print(i)
+        if (i["lastname"] == nachname and i["firstname"] == vorname):
+            print()
+
+            return i
+    else:
+        return {}
