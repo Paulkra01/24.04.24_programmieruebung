e6f42d27b7ac2e6e69124acdbb5ee815cb7438c1
Paulkra01 <krakis007@gmail.com>
2024-05-08 12:12:36 +0200

    update read_data

--- 3/read_data.py
+++ 3/read_data.py
@@ -1,7 +1,29 @@
 import csv
 import pandas as pd
+import numpy as np
+
+def readCSV():
+    # Create DataFrame
+    # df = pd.read_csv(data, sep =";")
+    df = pd.read_csv("activities/activity.csv", sep =",")
+
+    duration = df["Duration"]
+    Distance = df["Distance"]
+    originalPace = df["OriginalPace"]
+    heartrate = df["HeartRate"]
+    cadence = df["Cadence"]
+    powerOriginal = df["PowerOriginal"]
+    CalculatedPace = df["CalculatedPace"]
+    return df
+
+def dataAnalysis():
+    pass
+
+def createFigure():
+    pass
 
 
-# Create DataFrame
-df = pd.read_csv("Data/data.csv", sep =";")
+# df["Time"] = None
+# df["PowerOriginal"].max()
 
+print(readCSV())
