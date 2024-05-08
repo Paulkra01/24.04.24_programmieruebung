9804b60034c9914567a5192762b768f1a99f5bd3
Paulkra01 <krakis007@gmail.com>
2024-05-08 11:32:13 +0200

    add picture in main file

--- 2/main.py
+++ 2/main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import streamlit as st
-import read_data
+import read_data as rd
 
 
 
@@ -21,23 +21,25 @@ with col2:
 if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
 
-person_names = read_data.get_person_list()
+person_names = rd.get_person_list()
 
 
 st.session_state.current_user = st.selectbox('Versuchsperson', options = person_names, key="sbVersuchsperson")
 
 st.write("Der Name ist: ", st.session_state.current_user)
 
+print(st.session_state.current_user)
 
 # Auslesen des Pfades aus dem zurückgegebenen Dictionary
-current_picture_path = st.session_state.current_user["picture_path"]
+current_picture_path = rd.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+
 
 if 'picture_path' not in st.session_state:
     st.session_state.picture_path = 'data/pictures/none.jpg'
 
 # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
 if st.session_state.current_user in person_names:
-    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+    st.session_state.picture_path = rd.find_person_data_by_name(st.session_state.current_user)["picture_path"]
 
 
 # Öffne das Bild und Zeige es an
