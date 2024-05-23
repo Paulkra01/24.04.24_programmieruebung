d6b1db9273f4eb48c4bc389aaa07ffd0ce5331e5
Paulkra01 <krakis007@gmail.com>
2024-05-23 23:47:25 +0200

    add create_plots file

--- 2/create_plots.py
+++ 2/create_plots.py
@@ -1,11 +1,3 @@
-import csv
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-
-
 def readCSV():
     # Create DataFrame
     # df = pd.read_csv(data, sep =";")
