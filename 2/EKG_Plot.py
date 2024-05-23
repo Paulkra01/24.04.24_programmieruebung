118dcfa89d4c5233d283d4ea349507f14287f39e
Paulkra01 <krakis007@gmail.com>
2024-05-23 23:23:59 +0200

    update ekg_plot

--- 2/EKG_Plot.py
+++ 2/EKG_Plot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def readCSV():
@@ -35,11 +36,56 @@ def createFigure():
     heart_rate = df["HeartRate"]  
     power_original = df["PowerOriginal"]  
 
-    fig = px.line(df, x=time, y=[power_original, heart_rate])
+    # fig = px.line(df, x=time, y=[power_original, heart_rate])
 
-    fig.update_traces(line_color='blue', name='Leistung', selector=dict(name='power_original'))
-    fig.update_traces(line_color='red', name='Herzfrequenz', selector=dict(name='heart_rate'))
-    fig.update_layout(yaxis_range=[0, power_original.max()])
+    # fig.update_traces(line_color='blue', name='Leistung', selector=dict(name='power_original'))
+    # fig.update_traces(line_color='red', name='Herzfrequenz', selector=dict(name='heart_rate'))
+    # fig.update_layout(yaxis_range=[0, power_original.max()])
+
+    # fig.update_layout(
+    #     xaxis_title='Time / s',
+    #     yaxis=dict(
+    #         title='Power / W',
+    #         titlefont=dict(color='blue'),
+    #         tickfont=dict(color='blue')
+    #     ),
+    #     yaxis2=dict(
+    #         title='heartrate / bpm',
+    #         titlefont=dict(color='red'),
+    #         tickfont=dict(color='red'),
+    #         overlaying='y',
+    #         side='right'
+    #     )
+    # )
+
+    fig = go.Figure()
+
+    # Hinzufügen der PowerOriginal-Linie
+    fig.add_trace(go.Scatter(x=time, y=df['PowerOriginal'], mode='lines', name='PowerOriginal',
+                            yaxis='y1'))
+
+    # Hinzufügen der HeartRate-Linie
+    fig.add_trace(go.Scatter(x=time, y=df['HeartRate'], mode='lines', name='HeartRate',
+                            yaxis='y2'))
+
+    # Layout aktualisieren, um die sekundäre y-Achse zu unterstützen
+    fig.update_layout(
+        title='Power and Heart Rate over Time',
+        xaxis_title='Time',
+        yaxis_title='PowerOriginal',
+        yaxis2=dict(
+            title='HeartRate',
+            overlaying='y',
+            side='right'
+        ),
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        )
+    )
+
 # limit power
 
 # max heart rate 
@@ -54,7 +100,7 @@ def createFigure():
         zone_times.append(zone_time)
         fig.add_shape(type="rect",
                       xref="paper",
-                      yref="y",
+                      yref="y2",
                       x0=0,
                       y0=heart_rate_zones[i],
                       x1=1,
@@ -74,5 +120,9 @@ def createFigure():
 
 #Plot anzeigen
     fig.show()
+
+
+
+
 print(createFigure())
 print(dataAnalysis())
