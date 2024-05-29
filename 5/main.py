d79d648d091d46b97d17d3c07b1417fee890f1cd
Paulkra01 <krakis007@gmail.com>
2024-05-29 09:39:27 +0200

    add folder 5

---
+++ 5/main.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from PIL import Image
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def load_by_id():
+    pass
+
+def find_peaks():
+    pass
