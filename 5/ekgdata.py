6a3ab50ca8fa065702ef499e917c3c17b047676f
jannis-chr <jannis.pohl@gmx.de>
2024-05-29 11:23:52 +0200

    upadate_ekgdata

--- 5/ekgdata.py
+++ 5/ekgdata.py
@@ -1,12 +1,60 @@
 import json
 import pandas as pd
-import plotly.express as px
+import numpy as np
+import plotly as pl
 
 # %% Objekt-Welt
 
 # Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
 
 class EKGdata:
+
+    @staticmethod
+    def load_by_id():
+                file = open("data/person_db.json")
+                person_data = json.load(file)
+                for person in person_data:
+                    for ekg_test in person["ekg_tests"]:
+                        if ekg_test["id"] == ekg_id:
+                            ekg = EKGdata(ekg_test)
+                        return ekg	
+
+    @staticmethod
+    def find_peaks(person_data, threshold, respacing_factor=5):
+        """
+        A function to find the peaks in a series
+        Args:
+            - series (pd.Series): The series to find the peaks in
+            - threshold (float): The threshold for the peaks
+            - respacing_factor (int): The factor to respace the series
+        Returns:
+            - peaks (list): A list of the indices of the peaks
+        """
+        file = open("data/person_db.json")
+        person_data = json.load(file)
+
+       
+        # Respace the series
+        person_data = person_data.iloc[::respacing_factor]
+        
+        # Filter the series
+        person_data = person_data[person_data>threshold]
+
+
+        peaks = []
+        last = 0
+        current = 0
+        next = 0
+
+        for index, row in person_data.items():
+            last = current
+            current = next
+            next = row
+
+            if last < current and current > next and current > threshold:
+                peaks.append(index-respacing_factor)
+
+        return peaks
 
 ## Konstruktor der Klasse soll die Daten einlesen
 
@@ -33,3 +81,31 @@ if __name__ == "__main__":
     print(ekg_dict)
     ekg = EKGdata(ekg_dict)
     print(ekg.df.head())
+
+
+
+# %% Funktionen
+
+        
+@staticmethod
+def find_peaks():
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    for person in person_data:
+            for ekg_test in person["ekg_tests"]:
+                    if ekg_test["id"] == id:
+                        ekg = EKGdata(ekg_test)
+                        peaks = find_peaks(ekg.df['Messwerte in mV'], distance=100)
+                        ekg.df['Peaks'] = np.nan
+                        ekg.df.loc[peaks, 'Peaks'] = ekg.df.loc[peaks, 'Messwerte in mV']
+                        peaks_list = ekg.df['Peaks'].tolist()
+
+                    return " das sind die peaks" + peaks_list
+
+@staticmethod
+def estimate_hr():
+    pass
+
+@staticmethod
+def plot_time_series():
+    pass
